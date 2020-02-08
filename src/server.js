1034bfd1e4a38e3d04b71ac9669b2a5d869a2859
Katie Roy <kmr8172@g.rit.edu>
2020-02-07 23:43:29 -0500

    cleaned up code using eslint and fixed eslint errors

--- src/server.js
+++ src/server.js
@@ -1,41 +1,39 @@
 const http = require('http');
 const url = require('url');
+const query = require('querystring');
 const htmlHandler = require('./htmlResponses.js');
 const apiHandler = require('./apiHandler.js');
-const query = require('querystring');
 
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
 const urlStruct = {
-    GET: {
-        '/': htmlHandler.getIndex,
-        '/style.css': htmlHandler.getCSS,
-        '/success': apiHandler.getSuccess,
-        '/badRequest': apiHandler.getBadRequest,
-        '/unauthorized': apiHandler.getUnauthorized,
-        '/forbidden': apiHandler.getForbidden,
-        '/internal': apiHandler.getInternal,
-        '/notImplemented': apiHandler.getNotImplemented,
-        notFound: apiHandler.getNotFound,
-    }
+  GET: {
+    '/': htmlHandler.getIndex,
+    '/style.css': htmlHandler.getCSS,
+    '/success': apiHandler.getSuccess,
+    '/badRequest': apiHandler.getBadRequest,
+    '/unauthorized': apiHandler.getUnauthorized,
+    '/forbidden': apiHandler.getForbidden,
+    '/internal': apiHandler.getInternal,
+    '/notImplemented': apiHandler.getNotImplemented,
+    notFound: apiHandler.getNotFound,
+  },
 };
 
 const onRequest = (request, response) => {
-    const parsedUrl = url.parse(request.url);
+  const parsedUrl = url.parse(request.url);
 
-    if (urlStruct[request.method][parsedUrl.pathname]) {
-        if(!request.headers.accept || (request.headers.accept.split(',')[0] !== 'application/json' && request.headers.accept.split(',')[0] !== 'text/xml')){
-            urlStruct[request.method][parsedUrl.pathname](request, response, 'application/json', query.parse(parsedUrl.query));
-        } else {
-            urlStruct[request.method][parsedUrl.pathname](request, response, request.headers.accept.split(',')[0], query.parse(parsedUrl.query));
-        }
+  if (urlStruct[request.method][parsedUrl.pathname]) {
+    if (!request.headers.accept || (request.headers.accept.split(',')[0] !== 'application/json' && request.headers.accept.split(',')[0] !== 'text/xml')) {
+      urlStruct[request.method][parsedUrl.pathname](request, response, 'application/json', query.parse(parsedUrl.query));
     } else {
-        if(!request.headers.accept || (request.headers.accept.split(',')[0] !== 'application/json' && request.headers.accept.split(',')[0] !== 'text/xml')){
-            urlStruct[request.method].notFound(request, response, 'application/json', query.parse(parsedUrl.query));
-        } else {
-            urlStruct[request.method].notFound(request, response, request.headers.accept.split(',')[0], query.parse(parsedUrl.query));
-        }
+      urlStruct[request.method][parsedUrl.pathname](request, response, request.headers.accept.split(',')[0], query.parse(parsedUrl.query));
     }
+  } else if (!request.headers.accept || (request.headers.accept.split(',')[0] !== 'application/json' && request.headers.accept.split(',')[0] !== 'text/xml')) {
+    urlStruct[request.method].notFound(request, response, 'application/json', query.parse(parsedUrl.query));
+  } else {
+    urlStruct[request.method].notFound(request, response, request.headers.accept.split(',')[0], query.parse(parsedUrl.query));
+  }
 };
 
 http.createServer(onRequest).listen(port);
